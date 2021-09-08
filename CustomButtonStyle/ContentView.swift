d41b1f449401ba3567b486e404fadf2a4e152b1e
Frederick Javalera <fjavalera@goldpointsystems.com>
2021-09-08 16:35:13 -0600

    init

---
+++ CustomButtonStyle/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CustomButtonStyle
+//
+//  Created by Frederick Javalera on 9/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CustomButtonStyle/CustomButtonStyleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomButtonStyleApp.swift
+//  CustomButtonStyle
+//
+//  Created by Frederick Javalera on 9/8/21.
+//
+
+import SwiftUI
+
+@main
+struct CustomButtonStyleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
